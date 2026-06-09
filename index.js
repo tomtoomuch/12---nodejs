8aa4e38bac79c36f719c91b915adb8a9cb0bc7a2
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-09 11:24:58 +0200

    3 structures de contrôle de flux

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 let message = "Hello World World !";
 console.log(message);
+
+ageUtilisateur = 34;
+if (ageUtilisateur >= 18) {
+    if (ageUtilisateur > 65) {
+        console.log("Vous êtes vieux !");
+    } else {
+        console.log("Vous êtes adulte !");
+    }
+} else {
+    console.log("Vous êtes mineur !");
+}
+
+let compteur = 10;
+console.log(compteur);
+while (compteur > 0) {
+    compteur --;
+    console.log(compteur);
+}
+
+prenom = "Thomas";
+function direBonjour(prenom) {
+    console.log("Bonjour " + prenom + " !");
+}
+
+direBonjour(prenom);
+
