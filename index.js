bbccabbe044e147e1a19d8513a27382501913553
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-09 15:53:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let message = "Hello World World !";
-console.log(message);
+let messageBienvenue = "Hello World World !";
+console.log(messageBienvenue);
 
 ageUtilisateur = 34;
 if (ageUtilisateur >= 18) {
@@ -38,3 +38,56 @@ const calculSurface = (longueur,largeur) => {
     return surface;
 }
 console.log(calculSurface(13,17));
+
+
+setTimeout( () => {console.log("une seconde écoulée"); }, 1000 );
+
+function message() {
+  console.log("un message");
+}
+
+setTimeout( message, 1000 );
+
+
+let personnages = [
+    {"nom": "David", "age": 42, "taille": 192, "membre": true},
+    {"nom": "José", "age": 23, "taille": 165, "membre": false},
+    {"nom": "Agnès", "age": 36, "taille": 152, "membre": true}
+];
+
+for (let personnage of personnages) {
+    console.log("Je m'appelle", personnage.prenom, personnage.nom, "j'ai ", personnage.age , " ans.");
+}
+
+
+let users = [
+    {
+        "prenom": "René",
+        "nom": "LaTaupe",
+        "age": 49
+    },
+    {
+        "prenom": "Michel",
+        "nom": "Sardou",
+        "age": 65
+    },
+    {
+        "prenom": "jean",
+        "nom": "Valjean",
+        "age": 38
+    }
+];
+
+const userPlusQuaranteAns = users.filter((user) => user.age >= 40)
+
+console.log(userPlusQuaranteAns)
+
+// .map() parcourt une liste en contrôlant un predicat
+
+let vieux = [];
+for (let user of users) {
+    if (user.age > 40) {
+        vieux.push(user);
+        };
+    }
+console.log(vieux);
